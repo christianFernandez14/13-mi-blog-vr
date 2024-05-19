d7b6702cda87dd75caa06c093d1bcc6d55c84d0b
Christian Fernandez <christian14@gmail.com>
2024-05-19 14:12:30 -0400

    Diseño de la primera ruta util / definiendo los pasos que debo hacer en el metodo de crear  .178 P2

--- Api/controllers/articulo.js
+++ Api/controllers/articulo.js
@@ -1,6 +1,18 @@
 
 const crear = (req, res) => {
 
+  // Pasos para enviar info a la DB:
+  // Recoger la información por post a guardar, esto viene desde el frondEnd (formulario, petición )
+
+  // Validar los datos (aca hacemos uso de la libreria validator y nos evitamos la complejidad de las validaciones)
+
+  // Debemos crear el Obejeto que sera guardado, bajo el Schema ya diseñado es decir el modelo creado
+
+  // Asignamos los valores de manera al objeto (de manera manual o automatizada)
+
+  // Guardar el articulo en la base de dato
+
+  // Devolver el resultado
   return res.status(200).json({
     status: "success"
   })
