198deddd84d86bec67069b8ea24075ccce86b721
Christian Fernandez <christian14@gmail.com>
2024-05-21 08:41:30 -0400

    EndPoint para editar un articulo / modelo finalizado .185 P2

--- Api/controllers/articulo.js
+++ Api/controllers/articulo.js
@@ -2,10 +2,49 @@ const validator = require("validator")
 const Articulo = require("../models/Articulo")
 
 
-const editarArticulo = (req, res) =>{
+const editarArticulo = (req, res) => {
 
-  return res.status(200).json({
-    status: "success"
+  // Recoger el id del articulo a editar
+  let articuloId = req.params.id
+
+
+  // Recoger los datos del body
+  let parametros = req.body
+
+  // Validar los datos
+  try {
+
+    let validar_titulo = !validator.isEmpty(parametros.titulo) &&
+      validator.isLength(parametros.titulo, { min: 5, max: undefined })
+
+    let validar_contenido = !validator.isEmpty(parametros.contenido)
+
+    if (!validar_titulo || !validar_contenido) {
+      throw new Error("No se ha validado la información")
+    }
+
+  } catch (error) {
+    return res.status(400).json({
+      status: "error",
+      message: "Error al actulizar el articulo"
+    })
+  }
+
+  // Buscar y actulizar datos
+  Articulo.findOneAndUpdate({ _id: articuloId }, parametros, { new: true }, (error, articuloEditado) => {
+
+    if (error || !articuloEditado) {
+      return res.status(404).json({
+        status: "error",
+        menssage: "No se ha podido editar el articulo"
+      })
+    }
+    // Devolver respuesta.
+    return res.status(200).json({
+      status: "success",
+      artiuculo: articuloEditado,
+      menssage: "Articulo editado con exito ..."
+    })
   })
 }
 
@@ -13,7 +52,7 @@ const eliminarArticulo = (req, res) => {
 
   let articulo_id = req.params.id
 
-  Articulo.findOneAndDelete({_id: articulo_id}, (error, articuloEliminar) => {
+  Articulo.findOneAndDelete({ _id: articulo_id }, (error, articuloEliminar) => {
 
     if (error || !articuloEliminar) {
       return res.status(404).json({
